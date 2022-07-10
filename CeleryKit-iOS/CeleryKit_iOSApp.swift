169a3f20a78b0689ddb14ba69561d76e46b3aa1b
samirafisaleh <sami.rafi.saleh@gmail.com>
2022-07-09 17:45:12 -0700

    Initial commit

---
+++ CeleryKit-iOS/CeleryKit_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  CeleryKit_iOSApp.swift
+//  CeleryKit-iOS
+//
+//  Created by Sami Saleh on 6/25/22.
+//
+
+import SwiftUI
+
+@main
+struct CeleryKit_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CeleryKit-iOS/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  CeleryKit-iOS
+//
+//  Created by Sami Saleh on 6/25/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
